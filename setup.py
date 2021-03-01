71eb0a42934bf5497e9ac27e52df23469d79dbb1
David Kirkby <dkirkby@uci.edu>
2021-02-28 19:18:53 -0800

    prepare to tag 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='desietc',
-    version='0.1.dev0',
+    version='0.1.0',
     description='Online exposure-time calculator for DESI',
     url='http://github.com/dkirkby/desietc',
     author='David Kirkby',
