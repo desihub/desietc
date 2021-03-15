3c2b2a0e6d2d270a6beb9e364fd1a257fe5290d6
David Kirkby <dkirkby@uci.edu>
2021-03-14 20:14:12 -0700

    comment out in reset

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
  - Calculate FFRAC for sbprof=ELG,BGS,FLT.
  - Periodic telemetry updates when the shutters are open.
+## Changed
+ - Request to stop/split is only sent once per exposure.
 ### Removed
  - Remove debug logging of GFA array view/copy.
 ### Fixed
--- desietc/online.py
+++ desietc/online.py
@@ -460,16 +460,15 @@ class OnlineETC():
             self.ETCalg.reset_counts()
             self.ETCalg.accum.reset()
             self.image_processing.clear()
-            # reset per-tile variables
-            self.img_start_time = None
-            self.img_stop_time = None
-            self.img_stop_src = None
+            #self.img_start_time = None
+            #self.img_stop_time = None
+            #self.img_stop_src = None
 
-        # reset per-exposure variables
-        self.expid = None
-        self.etc_start_time = None
-        self.etc_stop_time = None
-        self.etc_stop_src = None
+        # reset onlineETC variables
+        #self.expid = None
+        #self.etc_start_time = None
+        #self.etc_stop_time = None
+        #self.etc_stop_src = None
 
         # update status
         if update_status == True:
