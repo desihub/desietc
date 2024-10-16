6dae7839e8186b45dbe03fc90b23f335049808d9
David Kirkby <dkirkby@uci.edu>
2024-10-16 12:33:28 +0100

    prepare to tag 0.1.19

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup, find_packages
 
 
-setup(name='desietc',
-      version='0.1.18.dev347',  # MAKE SURE THIS MATCHES desietc/_version.py! There are also ways to automate this.
-      description='Online exposure-time calculator for DESI',
-      url='http://github.com/desihub/desietc',
-      author='David Kirkby',
-      author_email='dkirkby@uci.edu',
-      license='MIT',
-      packages=find_packages(exclude=["tests",]),
-      install_requires=['numpy', 'scipy', 'fitsio'],
-      include_package_data=False,
-      zip_safe=False,
-      entry_points = {'console_scripts': ['etcreplay=desietc.scripts.replay:main',
-                                          'etcdepth=desietc.scripts.depth:main',],})
+setup(
+    name="desietc",
+    version="0.1.19",  # MAKE SURE THIS MATCHES desietc/_version.py! There are also ways to automate this.
+    description="Online exposure-time calculator for DESI",
+    url="http://github.com/desihub/desietc",
+    author="David Kirkby",
+    author_email="dkirkby@uci.edu",
+    license="MIT",
+    packages=find_packages(
+        exclude=[
+            "tests",
+        ]
+    ),
+    install_requires=["numpy", "scipy", "fitsio"],
+    include_package_data=False,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "etcreplay=desietc.scripts.replay:main",
+            "etcdepth=desietc.scripts.depth:main",
+        ],
+    },
+)
